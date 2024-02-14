e90f89a802f72c4e470f9d8780e55cd2a22b46bf
xev <git@schore.org>
2024-02-14 16:09:33 -0500

    feat: added --test_messages option
    For saving the message for later debugging

--- src/imessage_email_alert/__init__.py
+++ src/imessage_email_alert/__init__.py
@@ -27,21 +27,21 @@ def process_messages(
 @click.command()
 @click.argument("buddy")
 @click.option(
-    "--credentials_file",
+    "--credentials-file",
     required=False,
     type=click.Path(exists=True),
     default=config_directory / ".credentials.json",
     help="The location of the credentials file",
 )
 @click.option(
-    "--token_file",
+    "--token-file",
     required=False,
     type=click.Path(exists=True),
     default=config_directory / ".token.json",
     help="The location to save the token file file",
 )
 @click.option(
-    "--log_dir",
+    "--log-dir",
     required=False,
     type=click.Path(exists=True),
     help="The location to save the log file",
@@ -49,11 +49,40 @@ def process_messages(
 @click.option(
     "--debug/--no-debug", default=False, help="Enable debugging to the console"
 )
-def imessage_email_alert(buddy, credentials_file, token_file, log_dir, debug):
+@click.option(
+    "--save-messages",
+    required=False,
+    type=click.Path(
+        exists=True,
+    ),
+    help="Directory to save original email messages to",
+)
+@click.option(
+    "--test-messages",
+    required=False,
+    type=click.Path(exists=True),
+    help="Directory containing previously saved messages to test",
+)
+def imessage_email_alert(
+    buddy, credentials_file, token_file, log_dir, debug, save_messages, test_messages
+):
     """Sends an iMessage alert to BUDDY (phone number or email) whenever you get an
     email"""
     if log_dir is not None:
         log_dir = Path(log_dir)
+
+    if save_messages is not None:
+        save_messages = Path(save_messages)
+        save_messages.mkdir(parents=True, exist_ok=True)
+        if not save_messages.is_dir():
+            print(f"'{save_messages}' is not a directory")
+            exit(1)
+
+    if test_messages is not None:
+        test_messages = Path(test_messages)
+        if not test_messages.is_dir():
+            print(f"'{test_messages}' is not a directory")
+            exit(1)
 
     imessage = iMessageEmailAlert(
         buddy,
@@ -61,8 +90,13 @@ def imessage_email_alert(buddy, credentials_file, token_file, log_dir, debug):
         token_file=token_file,
         log_dir=log_dir,
         debug=debug,
+        save_messages=save_messages,
+        test_messages=test_messages,
     )
-    imessage.process_messages()
+    if test_messages is not None:
+        imessage.test_messages()
+    else:
+        imessage.process_messages()
 
 
 if __name__ == "__main__":
