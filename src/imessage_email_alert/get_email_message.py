01a0e35bc880dd63c7ec012e5ff15d7ef1d32ef2
script <script@x-pro.lan>
2024-03-02 14:44:59 -0500

    Took out error

--- src/imessage_email_alert/get_email_message.py
+++ src/imessage_email_alert/get_email_message.py
@@ -148,26 +148,32 @@ class GetEmailMessage:
 
                 match part_mime_type:
                     case "text/plain":
-                        decoded_data = base64.urlsafe_b64decode(
-                            part["body"]["data"]
-                        ).decode("utf-8")
-                        text = decoded_data
+                        if part["body"].get("date") is not None:
+                            if part["body"]["data"] != "Unreadable message":
+                                decoded_data = base64.urlsafe_b64decode(
+                                    part["body"]["data"]
+                                ).decode("utf-8")
+                                text = decoded_data
 
                     case "text/html":
-                        decoded_data = base64.urlsafe_b64decode(
-                            part["body"]["data"]
-                        ).decode("utf-8")
-                        soup = BeautifulSoup(decoded_data, "html.parser")
-                        html_text = soup.get_text()
+                        if part["body"].get("date") is not None:
+                            if part["body"]["data"] != "Unreadable message":
+                                decoded_data = base64.urlsafe_b64decode(
+                                    part["body"]["data"]
+                                ).decode("utf-8")
+                                soup = BeautifulSoup(decoded_data, "html.parser")
+                                html_text = soup.get_text()
 
                     case "text":
-                        decoded_data = base64.urlsafe_b64decode(
-                            part["body"]["data"]
-                        ).decode("utf-8")
-                        text = decoded_data
-                        if decoded_data.startswith("<!DOCTYPE"):
-                            soup = BeautifulSoup(decoded_data, "html.parser")
-                            text = soup.get_text()
+                        if part["body"].get("date") is not None:
+                            if part["body"]["data"] != "Unreadable message":
+                                decoded_data = base64.urlsafe_b64decode(
+                                    part["body"]["data"]
+                                ).decode("utf-8")
+                                text = decoded_data
+                                if decoded_data.startswith("<!DOCTYPE"):
+                                    soup = BeautifulSoup(decoded_data, "html.parser")
+                                    text = soup.get_text()
                 pass
         return text, html_text
 
